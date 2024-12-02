7514a8010003c09f6eb519d1da8bd125a0d926a8
killerwife <killerwife@gmail.com>
2024-12-02 21:59:58 +0100

    Add all tbc herbs and veins

--- cmangos-web-api/sql/create/gameobject_zones.sql
+++ cmangos-web-api/sql/create/gameobject_zones.sql
@@ -1,6 +1,111 @@
+truncate gameobject_zone;
+
+-- herbs
 -- golden sansam
 INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
 (176583,3483),
 (176583,3521),
 (176583,3523);
+
+-- mountain silversage
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(176586,3483);
+
+-- blindweed
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(183046,3521);
+
+-- liferoot
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(2041,3523);
+
+-- hellfire spineleaf
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(181372,3483);
+
+-- felweed - generic
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(181270,3483),
+(181270,3518),
+(181270,3519),
+(181270,3520),
+(181270,3522),
+(181270,3523);
+
+-- felweed - zangarmarsh
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(183044,3521);
+
+-- dreaming glory - generic
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(181271,3483),
+(181271,3518),
+(181271,3519),
+(181271,3520),
+(181271,3522),
+(181271,3523);
+
+-- dreaming glory - zangarmarsh
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(183045,3521);
+
+-- ragveil
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(183043,3521);
+
+-- terocone
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(181277,3519),
+(181277,3520);
+
+-- nightmare vine
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(181280,3520);
+
+-- netherbloom
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(181279,3523);
+
+-- mana thistle
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(181281,3518),
+(181281,3519),
+(181281,3520),
+(181281,3522),
+(181281,3523);
+
+-- netherdust bush
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(185881,3520);
+
+-- veins
+
+-- fel-iron-deposit
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(181555,3483),
+(181555,3518),
+(181555,3519),
+(181555,3520),
+(181555,3521),
+(181555,3522),
+(181555,3523);
+
+-- adamantite-deposit
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(181556,3518),
+(181556,3519),
+(181556,3520),
+(181556,3521),
+(181556,3522),
+(181556,3523);
+
+-- nethercite-deposit
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(185877,3520);
+
+-- misc
+
+-- netherwing egg
+INSERT INTO gameobject_zone(Entry, ZoneId) VALUES
+(185915,3520);
 

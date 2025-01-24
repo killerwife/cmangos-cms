385e79a1de8463d7e07dcbe7e73ff02a55fe0503
killerwife <killerwife@gmail.com>
2025-01-24 15:08:59 +0100

    Make slotId signed byte

--- cmangos-web-api/Services/Migrations/20250124132516_20250124_SignedSlotId.Designer.cs
+++ cmangos-web-api/Services/Migrations/20250124132516_20250124_SignedSlotId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Services.Repositories;
 
@@ -11,9 +12,11 @@ using Services.Repositories;
 namespace Services.Migrations
 {
     [DbContext(typeof(CmsDbContext))]
-    partial class CmsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250124132516_20250124_SignedSlotId")]
+    partial class _20250124_SignedSlotId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ cmangos-web-api/Services/Migrations/20250124132516_20250124_SignedSlotId.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20250124_SignedSlotId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "refresh_token",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<uint>(
+                name: "Id",
+                table: "account_ext",
+                type: "int unsigned",
+                nullable: false,
+                oldClrType: typeof(uint),
+                oldType: "int unsigned")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "refresh_token",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<uint>(
+                name: "Id",
+                table: "account_ext",
+                type: "int unsigned",
+                nullable: false,
+                oldClrType: typeof(uint),
+                oldType: "int unsigned")
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
